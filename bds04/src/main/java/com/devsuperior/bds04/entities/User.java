4f65d3ebedb074945f4cb8334c8d1ce513afa039
auriceliof <auricelio.suporte@hotmail.com>
2023-02-18 22:28:53 -0300

    ADD bds04

---
+++ bds04/src/main/java/com/devsuperior/bds04/entities/User.java
@@ -0,0 +1,89 @@
+package com.devsuperior.bds04.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String email;
+	private String password;
+	
+	public User () {
+	}
+	
+	public User(Long id, String email, String password) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
