713212cfaf071721dafa2bd24eb2f93d20dffde3
auriceliof <auricelio.suporte@hotmail.com>
2023-02-20 12:33:51 -0300

    ASSOCIAÇÃO USER-ROLE

--- bds04/src/main/java/com/devsuperior/bds04/entities/User.java
+++ bds04/src/main/java/com/devsuperior/bds04/entities/User.java
@@ -1,17 +1,45 @@
 package com.devsuperior.bds04.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
-public class User implements Serializable{
+@Table(name = "tb_user")
+public class User implements UserDetails, Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(unique = true)
 	private String email;
 	private String password;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role", 
+		joinColumns = @JoinColumn(name = "user_id"),
+		inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
 	
 	public User () {
 	}
@@ -21,6 +49,37 @@ public class User implements Serializable{
 		this.id = id;
 		this.email = email;
 		this.password = password;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 
 	public Long getId() {
@@ -43,6 +102,10 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
+	public Set<Role> getRoles(){
+		return roles;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -60,6 +123,7 @@ public class User implements Serializable{
 		return Objects.equals(id, other.id);
 	}
 }
+
 
 
 
